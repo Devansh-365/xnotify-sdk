38092fbac11ddacf939d30b7349dcfe18e27d865
Devansh Tiwari <83155844+Devansh-365@users.noreply.github.com>
2024-08-30 15:47:56 +0530

    setup

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import type { SubSDK } from "./types";
+
+class XNotifySdk {
+  constructor() {}
+
+  hello() {
+    return "hello";
+  }
+}
+
+export default XNotifySdk;
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export interface SubSDK<T = undefined> {
+  hello(): string;
+}
